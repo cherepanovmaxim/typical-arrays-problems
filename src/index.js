cb14f59e5b7e1f2dc39e981563c81d05433ae614
cherepanovmaxim <42759942+cherepanovmaxim@users.noreply.github.com>
2021-02-14 14:32:14 -0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return array.sort((a, b) => a - b)[0];
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return array.sort((a, b) => b - a)[0];
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let total = 0;
 
-exports.avg = function avg (array) {
-  return 0;
-}
+        for (let i = 0; i < array.length; i++) {
+            total += array[i];
+        }
+
+        return total / array.length;
+    }
+};
